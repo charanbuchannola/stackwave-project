22da9ec25088302d8ce1e6258992348b212a7da0
charanbuchannola <buchannolacharan@gmail.com>
2025-04-25 20:18:13 +0530

    fix: Update Navbar component to use useNavigate for login and register button actions

--- frontend/src/components/ui/Navbar.jsx
+++ frontend/src/components/ui/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { useAuth } from "@/context/ContextAuth";
-
+import { useNavigate } from "react-router-dom";
 const Navbar = () => {
   const { user, logout } = useAuth();
-
+  const navigate = useNavigate()
   return (
     <nav className="w-full max-w-5xl flex justify-between items-center py-4 px-6">
       <div className="text-xl font-bold">StackClone</div>
@@ -22,10 +22,14 @@ const Navbar = () => {
           </>
         ) : (
           <>
-            <button className="px-4 py-2 rounded-lg bg-gray-700 hover:bg-gray-600">
+            <button className="px-4 py-2 rounded-lg bg-gray-700 hover:bg-gray-600"
+              onClick={navigate("/login")}
+            >
               Login
             </button>
-            <button className="px-4 py-2 rounded-lg bg-gray-700 hover:bg-gray-600">
+            <button className="px-4 py-2 rounded-lg bg-gray-700 hover:bg-gray-600"
+            onClick={navigate("/register")}
+            >
               Register
             </button>
           </>
