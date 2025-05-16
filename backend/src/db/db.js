8803d3173d46ad35db6989d024fc849576484a93
charanbuchannola <buchannolacharan@gmail.com>
2025-05-17 01:32:26 +0530

    fix: Update MongoDB connection string in db.js for direct access

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -2,8 +2,10 @@ const mongoose = require("mongoose");
 
 const connect = () => {
   mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => console.log("MongoDB is Connected..."))
+    .connect(
+      "mongodb+srv://buchannolacharan:ghpsNKrS7eVnXUV8@cluster0.olj9y.mongodb.net/stack_wave"
+    )
+    .then(() => console.log("MongoDB Connected..."))
     .catch((err) => console.error("Could not connect to MongoDB:", err));
 };
 
