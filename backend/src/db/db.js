f8311879d27054553a09e47d63b099f09f12136a
charanbuchannola <buchannolacharan@gmail.com>
2025-05-18 03:43:04 +0530

    fix: Update MongoDB connection string to use environment variable for security

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = () => {
   mongoose
-    .connect(
-      "mongodb+srv://buchannolacharan:ghpsNKrS7eVnXUV8@cluster0.olj9y.mongodb.net/stack_wave"
-    )
+    .connect(process.env.MONGODB_URI)
     .then(() => console.log("MongoDB Connected..."))
     .catch((err) => console.error("Could not connect to MongoDB:", err));
 };
